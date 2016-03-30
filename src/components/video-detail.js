2692e47626388406ccc45b704a95578708ca11fa
sreeni <sreeni.san1312@gmail.com>
2016-03-30 16:19:23 -0500

    feat - video detail component

--- src/components/video-detail.js
+++ src/components/video-detail.js
@@ -1,3 +1,32 @@
-/**
- * Created by sreen on 3/29/2016.
- */
+import React from 'react';
+
+const VideoDetail = ({video}) => {
+
+  if (!video) {
+
+    return <div>Loading..</div>
+
+  }
+
+  return (
+
+    <div className="col-md-8 video-detail">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${video.id.videoId}`}></iframe>
+      </div>
+      <div className="details">
+        <div>
+          {video.snippet.title}
+        </div>
+        <div>
+          {video.snippet.description}
+        </div>
+      </div>
+    </div>
+
+
+  );
+
+};
+
+export default VideoDetail;
