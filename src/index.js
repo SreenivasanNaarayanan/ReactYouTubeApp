cac91ff4fabad9c66706498f74096b656fcda2cd
sreeni <sreeni.san1312@gmail.com>
2016-03-30 16:20:32 -0500

    feat - app component with youtube API call and using all other components

--- src/index.js
+++ src/index.js
@@ -1,18 +1,57 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
 
 const API_KEY = 'AIzaSyBji3AffcdxHYGJTWSXtU1_dNbDfcSWCpw';
 
-const App = () => {
+class App extends Component {
 
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+  constructor(props) {
 
-};
+    super(props);
+
+    this.state = {
+      selectedVideo: null,
+      videos: []
+    };
+
+    this.videoSearch('javascript');
+
+  }
+
+  videoSearch(term) {
+
+    YTSearch({
+      key: API_KEY,
+      term: term
+    }, (videos) => this.setState({
+      videos: videos,
+      selectedVideo: videos[0]
+    }));
+  }
+
+  render() {
+
+    const videoSearch = _.debounce(term => this.videoSearch(term), 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+
+  }
+
+}
 
 ReactDom.render(<App />, document.querySelector('.container'));
