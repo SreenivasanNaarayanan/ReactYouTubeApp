03db7e1ccfe23c7cd9c79017a1d16ae905779019
sreeni <sreeni.san1312@gmail.com>
2016-03-30 16:19:44 -0500

    feat - search bar component

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -2,9 +2,37 @@ import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
 
+  static displayName = 'search-bar';
+
+  static proptypes = {};
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = {term: ''};
+
+    this.onInputChange = this.onInputChange.bind(this);
+
+  }
+
+  onInputChange(term) {
+
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+
+  }
+
   render() {
 
-    return <input />
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)}
+        />
+      </div>
+    );
 
   }
 
